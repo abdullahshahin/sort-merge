354ce7a1bb0731eed9b90c0e8151800f9fc4e3f0
Abdullah Shahin <abdullah.shahin@arabiaweather.com>
2015-11-27 15:44:40 +0000

    initial release

---
+++ app.js
@@ -0,0 +1,6 @@
+exports.MergeMultiSync = require('./lib/twoD/multidSync');
+exports.MergeMultiAsync = require('./lib/twoD/multidAsync');
+exports.merge2dSync = require('./lib/twoD/2dSync');
+exports.merge2dAsync = require('./lib/twoD/2dAsync');
+exports.merge1dSync = require('./lib/oneD/1dSync');
+exports.merge1dAsync = require('./lib/oneD/1dAsync');
